c0919ee0f44f8df3f86e50e8fd8d34855dd6f9a0
ankitrhll9 <ankitrhll9@gmail.com>
2020-05-16 22:47:48 +0530

    doc: made the fisrt change as a comment

--- bikeshare.py
+++ bikeshare.py
@@ -6,6 +6,8 @@ CITY_DATA = { 'chicago': 'chicago.csv',
               'new york city': 'new_york_city.csv',
               'washington': 'washington.csv' }
 
+#making the first change in this file to pass in udacity review              
+
 def get_filters():
     """
     Asks user to specify a city, month, and day to analyze.
